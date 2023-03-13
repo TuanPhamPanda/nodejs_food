937243fa3d62ef5dc597c50a1ecef31808dde38a
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-03-14 01:40:20 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_restaurant"
+});
+
+
+db.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
+
+export default db;
